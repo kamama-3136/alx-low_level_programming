6da21be3f61b15a9726ee45c1bde687b931ac937
Jemimah kamama <122855283+kamama-3136@users.noreply.github.com>
2023-02-17 10:25:37 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry
- * 
- * Description 'Output to the standard error'
- * 
- * Return: Always 1
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
-int  main(void)
+int main(void)
 {
-	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
-
 
