8b2353ca796306d2c1350c3d17dc9e48e2a0e9f8
Your Name <kamamajemimah107.com>
2023-02-21 14:23:21 -0800

    task 16

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,16 +5,12 @@
  *
  * Return: Always 0.
  */
-int _main(void)
+int _islower(int c)
 {
 int r;
-
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
 return (0);
 }
