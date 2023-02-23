28985076d06e9c9cb8dbca82eb79e70156657b67
Your Name <kamamajemimah107.com>
2023-02-23 14:33:52 -0800

    My first commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
-
 /**
-* _isupper - check if char is uppercase
-* @c: is the char to be checked
-* Return: 1 if char is uppercase, otherwise 0.
+* _isupper - uppercase letters
+* @c: char to check
+*
+* Return: 0 or 1
 */
-
 
 int _isupper(int c)
 {
