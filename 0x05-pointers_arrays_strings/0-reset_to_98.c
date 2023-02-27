a44e9197bd605a32992f2456caaf20aa92701580
Your Name <kamamajemimah107.com>
2023-02-27 01:34:04 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* reset_to_98- takes a pointer to an int as parameter and updates the value 
+* reset_to_98- takes a pointer to an int as parameter and updates the value
 * @n: input
 * Return:n
 */
